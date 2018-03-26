5b6bbb995959826cab823e39e58ad63bb1907ba8
Jeff Mason <masonjeffreys@gmail.com>
2018-03-26 15:27:22 -0400

    FixNameOfMigration

--- db/migrate/20180308215701_add_discount_deadline_to_sale_prices.rb
+++ db/migrate/20180308215701_add_discount_deadline_to_sale_prices.rb
@@ -1,4 +1,4 @@
-class AddDisplayTextToSalePrices < ActiveRecord::Migration
+class AddDiscountDeadlineToSalePrices < ActiveRecord::Migration
   def change
     add_column :spree_sale_prices, :discount_deadline, :bool, default: false
     add_index :spree_sale_prices, :discount_deadline
